97164941a9cfd23d5151e2b0abefd523915b1bfc
Darren King <darrenbrettking@hotmail.com>
2016-01-06 15:39:35 -0800

    Add podcast controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,8 @@ Rails.application.routes.draw do
 # root 'static_pages#index'
 
 root 'welcome#index'
- 
-  match '/podcasts', to: 'static_pages#podcasts', via: 'get'
-  match '/podcast_show', to: 'static_pages#podcast_show', via: 'get'
-  match '/episode', to: 'static_pages#episode', via: 'get'
-  match '/dashboard', to: 'static_pages#dashboard', via: 'get'
-  match '/sign_in', to: 'static_pages#sign_in', via: 'get'
-  match '/sign_up', to: 'static_pages#sign_up', via: 'get'
-  match '/', to: 'static_pages#index', via: 'get'
+
+  
+  resources :podcasts, only: [:index, :show]
 
 end
