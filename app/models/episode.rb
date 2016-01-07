19e33201c7199192a0304c0b8be4022c2625e103
Darren King <darrenbrettking@hotmail.com>
2016-01-07 11:52:25 -0800

    Added mp3 uploads to epis

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,6 +1,9 @@
 class Episode < ActiveRecord::Base
-  belongs_to :podcast
-  
-  has_attached_file :episode_thumbnail, styles: { :large => "1000x1000#", :medium => "500x500#" }
-  validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\Z/
-end
+    belongs_to :podcast
+    
+    has_attached_file :episode_thumbnail, styles: { :large => "1000x1000#", :medium => "500x500#" }
+    validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\Z/
+    
+    has_attached_file :mp3
+    validates_attachment :mp3, :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] }, :file_name => { :matches => [/mp3\Z/] }
+  end
